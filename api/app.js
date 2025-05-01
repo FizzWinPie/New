bdb16d8aacfd42fe1e6776354f0cb70694925d1e
FizzWinPie <sulixhoni2000@gmail.com>
2025-04-30 21:52:19 -0400

    finished login/logout/register

---
+++ api/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js"
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(8800, () => {
+  console.log("Server is running");
+});
