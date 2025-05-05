4c39995be2a9acec6f4d20cb1f0979463d8f1fc2
FizzWinPie <sulixhoni2000@gmail.com>
2025-05-04 23:33:19 -0400

    finish notification

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,11 +2,19 @@ import { useContext, useState } from "react";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
+import {useNotificationStore} from "../../lib/notificationStore";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
 
   const { currentUser } = useContext(AuthContext);
+
+  const fetch = useNotificationStore(state => state.fetch);
+  const number = useNotificationStore(state => state.number);
+
+  if (currentUser) {
+    fetch();
+  }
 
   return (
     <nav>
@@ -22,14 +30,14 @@ function Navbar() {
       </div>
       <div className="right">
         {currentUser ? (
-          <div className="user" onClick={() => (window.location.href = "/profile")}>
-            <img
-              src={currentUser.avatar || "/noavatar.jpg"}
-              alt=""
-            />
+          <div
+            className="user"
+            onClick={() => (window.location.href = "/profile")}
+          >
+            <img src={currentUser.avatar || "/noavatar.jpg"} alt="" />
             <span>{currentUser.username}</span>
             <Link to="/profile" className="profile">
-              <div className="notification">3</div>
+              {number > 0 && <div className="notification">{number}</div>}
               <span>Profile</span>
             </Link>
           </div>
