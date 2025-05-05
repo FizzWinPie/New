b13a29b5fd5e639d769223ecc7d867a783dc734e
FizzWinPie <sulixhoni2000@gmail.com>
2025-05-05 00:53:16 -0400

    added 3000 to api origin cors

--- api/app.js
+++ api/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authRoute from "./routes/auth.route.js";
-import postRoute from "./routes/post.route.js"
+import postRoute from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import testRoute from "./routes/test.route.js";
@@ -12,7 +12,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production" ? process.env.CLIENT_URL : "http://localhost:5173",
+    origin: [
+      process.env.NODE_ENV === "production"
+        ? process.env.CLIENT_URL
+        : "http://localhost:5173",
+      "http://localhost:3000",
+    ],
     credentials: true,
     exposedHeaders: ["set-cookie"],
   })
